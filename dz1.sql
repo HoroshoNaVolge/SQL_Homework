afa482b383150b6eba7b26157e10bdf5f2141846
HoroshoNaVolge <44640207+HoroshoNaVolge@users.noreply.github.com>
2023-05-03 11:47:26 +0300

    Add DZ1
    Запросы по первой ДЗ

---
+++ dz1.sql
@@ -0,0 +1,10 @@
+USE myfirstdb;
+SELECT * FROM mobile_phones;
+
+SELECT product_name,manufacturer,price
+FROM mobile_phones
+WHERE product_count >2;
+
+SELECT *
+FROM mobile_phones
+WHERE manufacturer='Samsung';
